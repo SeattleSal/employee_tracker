387b30f6e00152214f1c06c9b0ea5bc159889259
Sally Perez <seattlesally1@gmail.com>
2021-01-03 11:43:50 -0700

    updated seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -2,27 +2,27 @@ USE employeesDB;
 
 insert into department (name)
 values ('Sales'),
-('Engineering'),
 ('Finance'),
-('Legal');
+('Human Resources'),
+('Quality Assurance');
 
 insert into role (title, salary, department_id)
-values('Sales Lead', 100000, 1),
-('Salesperson', 800000,1),
-('Lead Engineer', 150000, 2),
-('Software Engineer', 120000, 2),
-('Account Manager', 160000, 3),
-('Accountant', 125000, 3),
-('Legal Team Lead', 250000, 4),
-('Lawyer', 190000, 4);
+values('Branch Manager', 100000, 1),
+('Salesperson', 80000,1),
+('Receptionist', 55000, 1),
+('Head of Accounting', 80000, 2),
+('Accountant', 60000, 2),
+('HR Representative', 80000, 3),
+('Quality Assurance Director', 100000, 4);
 
 insert into employee (first_name, last_name, role_id, manager_id)
 values
-('John', 'Doe', 1, null),
-('Mike', 'Chen', 2, 1),
-('Ashley', 'Rodriguez', 3, NULL),
-('Kevin', 'Tupik', 4, 3),
-('Kumal', 'Singh', 5, null),
-('Malia', 'Brown', 6, 5),
-('Sarah', 'Lourd', 7, null),
-('Tom', 'Allen', 8, 7);
+('Michael', 'Scott', 1, null),
+('Jim', 'Halpert', 2, 1),
+('Dwight', 'Schrute', 2, 1),
+('Pam', 'Beesly', 3, null),
+('Angela', 'Martin', 4, null),
+('Kevin', 'Malone', 5, 5),
+('Oscar', 'Martinez', 5, 5),
+('Toby', 'Flenderson', 6, null),
+('Creed', 'Bratton', 7, null);
